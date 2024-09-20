5a2c8b726fac4e0c44f3c9e9fb67436c8262a7f9
mappu <mappu04@gmail.com>
2024-09-20 18:51:47 +1200

    uic: add main flag handling, add go:generate comment with preserved flags

--- cmd/miqt-uic/main.go
+++ cmd/miqt-uic/main.go
@@ -2,33 +2,48 @@ package main
 
 import (
 	"encoding/xml"
+	"flag"
 	"fmt"
 	"io/ioutil"
+	"os"
+	"strings"
 )
 
 func main() {
-	in, err := ioutil.ReadFile(`test.ui`)
+	inFile := flag.String("InFile", "", "Input .ui file")
+	outFile := flag.String("OutFile", "-", "Output .go file, or - for stdout")
+	packageName := flag.String("Package", "main", "Custom package name")
+	flag.Parse()
+
+	if *inFile == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	inXml, err := ioutil.ReadFile(*inFile)
 	if err != nil {
 		panic(err)
 	}
 
 	var parsed UiFile
-	err = xml.Unmarshal(in, &parsed)
+	err = xml.Unmarshal(inXml, &parsed)
 	if err != nil {
 		panic(err)
 	}
 
-	ret, err := xml.MarshalIndent(parsed, "", "  ")
+	gosrc, err := generate(*packageName, strings.Join(os.Args[1:], " "), parsed)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(string(ret))
+	if *outFile == "-" {
+		fmt.Println(string(gosrc))
 
-	gosrc, err := generate("main", parsed)
-	if err != nil {
-		panic(err)
+	} else {
+
+		err = ioutil.WriteFile(*outFile, gosrc, 0644)
+		if err != nil {
+			panic(err)
+		}
 	}
-
-	fmt.Println(string(gosrc))
 }
