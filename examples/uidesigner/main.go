16435f15659bbd36e19d95459e8eed7beff3e98a
mappu <mappu04@gmail.com>
2024-09-28 14:47:31 +1200

    examples/uidesigner: remove fmt package dependency

--- examples/uidesigner/main.go
+++ examples/uidesigner/main.go
@@ -1,20 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/mappu/miqt/qt"
 )
 
 func main() {
-
 	qt.NewQApplication(os.Args)
 
 	ui := NewMainWindowUi()
 	ui.MainWindow.Show()
 
 	qt.QApplication_Exec()
-
-	fmt.Println("OK!")
 }
