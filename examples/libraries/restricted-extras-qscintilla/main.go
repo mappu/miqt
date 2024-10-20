1ef63a16732dca7228d7e41edac82f8266efd71b
mappu <mappu04@gmail.com>
2024-10-20 18:00:04 +1300

    qscintilla: add example

---
+++ examples/libraries/restricted-extras-qscintilla/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"os"
+
+	"github.com/mappu/miqt/qt"
+	"github.com/mappu/miqt/qt-restricted-extras/qscintilla"
+)
+
+func main() {
+
+	qt.NewQApplication(os.Args)
+
+	area := qscintilla.NewQsciScintilla()
+	area.SetFixedSize2(640, 480)
+	area.Show()
+
+	qt.QApplication_Exec()
+}
