815d5bcc8b6a886ec5673722f461f8e8359b5fd1
mappu <mappu04@gmail.com>
2024-10-27 12:47:10 +1300

    qt6: rebuild (delete QGuiApplication::platform)

--- qt6/gen_qguiapplication_platform.cpp
+++
@@ -1,5 +0,0 @@
-#define WORKAROUND_INNER_CLASS_DEFINITION_QNativeInterface__QX11Application
-#include <qguiapplication_platform.h>
-#include "gen_qguiapplication_platform.h"
-#include "_cgo_export.h"
-
