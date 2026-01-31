17a8d4a58a8391334c75530a5ace5be265e095bd
Rick Calixte <10281587+rcalixte@users.noreply.github.com>
2026-01-31 13:20:11 -0500

    config-libraries.go: Remove SCXML due to current instability and clean up State Machine

--- cmd/genbindings/config-libraries.go
+++ cmd/genbindings/config-libraries.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"path/filepath"
 	"strings"
 )
@@ -604,7 +603,7 @@ func ProcessLibraries(clangBin, outDir, extraLibsDir string) {
 		ClangMatchSameHeaderDefinitionOnly,
 	)
 
-	// Qt 6 QStateMachine
+	// Qt 6 State Machine
 	// Depends on QtCore and QtQml
 	generate(
 		"qt6/statemachine",
@@ -614,28 +613,7 @@ func ProcessLibraries(clangBin, outDir, extraLibsDir string) {
 		},
 		AllowAllHeaders,
 		clangBin,
-		fmt.Sprintf("--std=c++17 %s %s",
-			pkgConfigCflags("Qt6StateMachine"),
-			pkgConfigCflags("Qt6StateMachineQml"),
-		),
-		outDir,
-		ClangMatchSameHeaderDefinitionOnly,
-	)
-
-	// Qt 6 SCXML
-	// Depends on QtCore and QtQml
-	generate(
-		"qt6/scxml",
-		[]string{
-			"/usr/include/x86_64-linux-gnu/qt6/QtScxml",
-			"/usr/include/x86_64-linux-gnu/qt6/QtScxmlQml",
-		},
-		AllowAllHeaders,
-		clangBin,
-		fmt.Sprintf("--std=c++17 %s %s",
-			pkgConfigCflags("Qt6Scxml"),
-			pkgConfigCflags("Qt6ScxmlQml"),
-		),
+		"--std=c++17 "+pkgConfigCflags("Qt6StateMachineQml"),
 		outDir,
 		ClangMatchSameHeaderDefinitionOnly,
 	)
